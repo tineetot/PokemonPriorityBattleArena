5a1e831d98552da3a6b1c6e4225785c78966bb3d
Kristine <detailzetsy@gmail.com>
2026-09-22 22:57:44 +0800

    Initialize project structure

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("       POKEMON PRIORITY BATTLE ARENA");
+        System.out.println("========================================");
+        System.out.println("Project setup successful!");
+    }
+}
